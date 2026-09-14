b3179575ea1b9bd27ddb720fe89c0a35295b5eac
teo2810 <teofalce@hotmail.it>
2026-09-14 21:12:44 +0200

    Logica app + cache sw con app.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = "menu-app-v3";
-const ASSETS = ["./index.html", "./manifest.json", "./icon.svg"];
+const ASSETS = ["./index.html", "./app.js", "./manifest.json", "./icon.svg"];
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
   self.skipWaiting();
