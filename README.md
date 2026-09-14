0bfbfa07f058942376675fcc5654f14c0c491f5a
teo2810 <teofalce@hotmail.it>
2026-09-14 21:11:02 +0200

    PWA Menu: file base, icona posate, service worker

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# menu
-PWA per menu nominati: importa da foto o file e consulta
+# Menu
+
+PWA per piu menu nominati. Importa da foto o file, correggi a mano, consulta oggi e le settimane.
+
+## Link
+
+- App: https://teo2810.github.io/menu/
+- Repo: https://github.com/teo2810/menu
+
+## Uso
+
+1. Apri il link sul telefono.
+2. Chrome, menu, Aggiungi a schermata Home.
+3. Tab Importa: nome del menu, foto o file.
+4. Controlla il testo riconosciuto e salva.
+5. In alto scegli quale menu e attivo.
---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE = "menu-app-v3";
+const ASSETS = ["./index.html", "./manifest.json", "./icon.svg"];
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener("activate", (e) => {
+  e.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener("fetch", (e) => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(caches.match(e.request).then((cached) => {
+    const net = fetch(e.request).then((res) => {
+      if (res.ok && e.request.url.startsWith(self.location.origin)) {
+        const copy = res.clone();
+        caches.open(CACHE).then((c) => c.put(e.request, copy));
+      }
+      return res;
+    }).catch(() => cached);
+    return cached || net;
+  }));
+});
